8accd9dd75843186fb1b601f44b4d66f270baeb5
Evely-27 <evelysantos27@gmail.com>
2022-05-30 23:58:28 -0300

    chamando Login da tela Inicio

--- app_mais_escola/src/layouts/inicio/Inicio.tsx
+++ app_mais_escola/src/layouts/inicio/Inicio.tsx
@@ -1,11 +1,16 @@
+import { useNavigation } from "@react-navigation/native";
 import React from "react";
 import { View, Text } from "react-native";
+import { TouchableOpacity } from "react-native-gesture-handler";
 import { stylesG } from "../../../assets/style/stylesGlobal";
 
 export default function Inicio(){
+    const navi = useNavigation();
     return (
         <View style={stylesG.background}>
             <Text>Tela Inicio </Text>
+            <TouchableOpacity style={{backgroundColor: "red"}} onPress = {() => { navi.navigate('Login' as never)}}>Login</TouchableOpacity>
+
         </View>
     )
 }
