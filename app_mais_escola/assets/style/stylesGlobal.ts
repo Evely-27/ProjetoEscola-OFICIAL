9d6cc2b1c42bb75ac49c1e9cc9e724eed9c2b6ed
Evely-27 <evelysantos27@gmail.com>
2022-05-30 23:12:00 -0300

    alterando App.tsx e criando arquivo de estilo global

---
+++ app_mais_escola/assets/style/stylesGlobal.ts
@@ -0,0 +1,11 @@
+import { StyleSheet } from 'react-native';
+
+
+ export const stylesG = StyleSheet.create({
+    background:{
+      flex:1,
+      alignItems: 'center',
+      justifyContent: 'center',
+      backgroundColor: '#1673d6'
+    }
+  });
