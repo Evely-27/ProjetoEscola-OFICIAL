863e6e6e68e9cd961095b54a20e008e433ecda9e
Evely-27 <evelysantos27@gmail.com>
2022-05-30 23:27:43 -0300

    preparando telas para usar o navigation

---
+++ app_mais_escola/src/layouts/list_schools/ListSchools.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+export default function ListSchool(){
+    return (
+        <View>
+            <Text>Tela lista de escolas </Text>
+        </View>
+    )
+}
---
+++ app_mais_escola/src/layouts/register/Register.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { View, Text } from "react-native";
+import { stylesG } from "../../../assets/style/stylesGlobal";
+
+export default function Register(){
+    return (
+        <View style={stylesG.background}>
+            <Text>Tela Cadastro</Text>
+        </View>
+    )
+}
