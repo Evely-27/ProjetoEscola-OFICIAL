48cc1f8f332615ddf01cec93db1d5547e78aa580
Evely-27 <evelysantos27@gmail.com>
2022-06-05 17:31:20 -0300

    chamando  lista de escolas no login para teste

--- app_mais_escola/src/layouts/login/Login.tsx
+++ app_mais_escola/src/layouts/login/Login.tsx
@@ -1,11 +1,15 @@
 import React from "react";
-import { View, Text } from "react-native";
+import { useNavigation } from "@react-navigation/native";
+import { View, Text, TouchableOpacity } from "react-native";
 import { stylesG } from "../../../assets/style/stylesGlobal";
 
 export default function Login(){
+    const navi = useNavigation();
     return (
         <View style={stylesG.background}>
             <Text>Tela Login </Text>
+            <TouchableOpacity style={{backgroundColor: "red"}} onPress = {() => { navi.navigate('ListSchools' as never)}}>Lista de Escolas</TouchableOpacity>
+
         </View>
     )
 }
