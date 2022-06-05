4ce1840c1cdc2dcaae711e4122126dfdea100acc
Evely-27 <evelysantos27@gmail.com>
2022-06-05 17:29:58 -0300

    adicionando lista de escolas nas rotas

--- app_mais_escola/src/routes/Routes.tsx
+++ app_mais_escola/src/routes/Routes.tsx
@@ -3,6 +3,7 @@ import { createStackNavigator } from "@react-navigation/stack";
 import { NavigationContainer } from '@react-navigation/native';
 import Inicio from '../layouts/inicio/Inicio';
 import Login from '../layouts/login/Login';
+import ListSchool from '../layouts/list_schools/ListSchools';
 
 export default function Routes() {
     const Stack = createStackNavigator();
@@ -11,6 +12,8 @@ export default function Routes() {
             <Stack.Navigator>
                 <Stack.Screen name='Inicio' component={Inicio} />
                 <Stack.Screen name='Login' component={Login} />
+                <Stack.Screen name='ListSchools' component={ListSchool} />
+
             </Stack.Navigator>
         </NavigationContainer>
     );
