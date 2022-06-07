840926cf5debb42ccebd41f13a9f3cecda7bcfe0
Evely-27 <evelysantos27@gmail.com>
2022-06-07 00:05:16 -0300

    tirando header de welcome

--- app_mais_escola/src/routes/Routes.tsx
+++ app_mais_escola/src/routes/Routes.tsx
@@ -10,7 +10,7 @@ export default function Routes() {
     return (
         <NavigationContainer>
             <Stack.Navigator>
-                <Stack.Screen name='Welcome' component={Welcome} />
+                <Stack.Screen name='Welcome' component={Welcome} options={{header: () => null}}/>
                 <Stack.Screen name='Login' component={Login} />
                 <Stack.Screen name='ListSchools' component={ListSchool} />
 
