b6494579f3bfbf3a952290b3378ffaeb7ab5d798
Evely-27 <evelysantos27@gmail.com>
2022-06-13 21:49:49 -0300

    comecando lista de escola-sem API

--- app_mais_escola/src/layouts/list_schools/ListSchool.tsx
+++ app_mais_escola/src/layouts/list_schools/ListSchool.tsx
@@ -1,11 +1,28 @@
 import React from "react";
-import { View, Text } from "react-native";
+import { View, Text, FlatList, TouchableOpacity, StyleSheet } from "react-native";
 import { stylesG } from "../../../assets/style/stylesGlobal";
+import PostSchools from "../../components/PostSchools";
+// import PostEscolas from "../../class/PostEscolas";
+
 
 export default function ListSchool(){
+    const listaescolas = [
+        {nomeEscola: "Escola Rosa Maria",rpa: "239b",quantAlun: 294},
+        {nomeEscola: "Escola Fernando sousa", rpa: "123",quantAlun: 653}
+    ]
     return (
         <View style={stylesG.background}>
             <Text>Tela lista de escolas </Text>
+
+
+        <FlatList
+            data={listaescolas}
+
+            renderItem={({item}) =>
+            <PostSchools postSc={item}></PostSchools> 
+            }>
+
+        </FlatList>
         </View>
     )
 }
