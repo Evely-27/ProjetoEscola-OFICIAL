5756e3455028e2a455d4ab58bc50aebfd7837cc5
Evely-27 <evelysantos27@gmail.com>
2022-06-13 21:50:15 -0300

    construtor para lista de escola

---
+++ app_mais_escola/src/class/ClassEscolas.ts
@@ -0,0 +1,9 @@
+export default class  ClassEscolas{
+
+    constructor(
+        
+        public id?: number,
+        public rpa: string= "",
+        public nomeEscola: string = "",
+        public quantAlun?: number ){}
+}
