01f31d53cf4d6ac73a7775261de351e727b6c92d
Evely-27 <evelysantos27@gmail.com>
2022-06-18 16:31:16 -0300

    criando parametros para passagem de variaveis por rotas

---
+++ app_mais_escola/src/class/ParametrosDasRotas.ts
@@ -0,0 +1,8 @@
+import { User } from "./User";
+
+export type ParametrosDasRotas = {
+    Welcome: undefined,
+    Login: undefined, 
+    Register:  undefined, 
+    ListSchool: User | undefined,
+}
