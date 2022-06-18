8153912c403768e6486fd40070e630986022bc29
Evely-27 <evelysantos27@gmail.com>
2022-06-18 17:13:39 -0300

    criando tela de reportar aluno

---
+++ app_mais_escola/src/layouts/report_student/ReportStudent.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { Text, View } from "react-native";
+
+export default function ReportStudent() {
+    return (
+        <View>
+            <Text>Tela de Reportar Aluno</Text>
+        </View>
+    )
+}
