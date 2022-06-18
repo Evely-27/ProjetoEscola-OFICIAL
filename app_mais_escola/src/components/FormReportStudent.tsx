7350bf272f617e1bf1bfebffb7bfb8b1dbf2f9cb
Evely-27 <evelysantos27@gmail.com>
2022-06-18 19:26:02 -0300

    criando componente form de reportar aluno

---
+++ app_mais_escola/src/components/FormReportStudent.tsx
@@ -0,0 +1,43 @@
+import React from "react";
+import { View, StyleSheet } from "react-native";
+import ComponentForm from "./ComponentForm";
+
+export default function FormReportStudent(){
+    return (
+        <View style={styles.form}>
+                <ComponentForm info="NOME COMPLETO:" exemplo="Digite seu nome"></ComponentForm>
+
+                    {/* informe do aluno : cpf*/}
+                 <ComponentForm info="CPF:" exemplo="Digite o CPF do aluno:"></ComponentForm>
+
+                        {/* informe do aluno : endereço*/}
+                <ComponentForm info="ENDEREÇO:" exemplo="Digite o endereço do aluno:"></ComponentForm>
+
+                <View style={styles.formlado}>
+                    <ComponentForm info="CEP:" exemplo="94856-97"></ComponentForm>
+                    <ComponentForm info="NÚMERO:" exemplo="856"></ComponentForm>
+                </View>
+
+                <ComponentForm info="TELEFONE" exemplo="(81) 9 65536-7564"></ComponentForm>
+                <ComponentForm info="MOTIVO" exemplo="motivo do aluno ser reportado:"></ComponentForm>
+                <ComponentForm info="MEDIDAS TOMADAS:" exemplo="Medidas tomadas para remediar a situação:"></ComponentForm>
+                </View>
+            
+    );
+}
+const styles = StyleSheet.create ({
+    container:{
+        flex: 1,
+        backgroundColor: '#fff',
+ 
+    },
+    form:{
+        margin: 3,
+        padding: 8,
+    },
+    formlado:{
+        flexDirection: 'row',
+        justifyContent:'space-between',
+
+    }
+})
