a942e0f21cbce5f801f843ff52611292ac70ed4d
Evely-27 <evelysantos27@gmail.com>
2022-06-18 19:27:30 -0300

    tela reportar aluno finalizada

--- app_mais_escola/src/layouts/report_student/ReportStudent.tsx
+++ app_mais_escola/src/layouts/report_student/ReportStudent.tsx
@@ -1,10 +1,59 @@
 import React from "react";
-import { Text, View } from "react-native";
+import {  StyleSheet, Text, View } from "react-native";
+import BottonForm from "../../components/BottonForm";
+import { FontAwesome5 } from '@expo/vector-icons';
+import FormReportStudent from "../../components/FormReportStudent";
 
 export default function ReportStudent() {
     return (
-        <View>
-            <Text>Tela de Reportar Aluno</Text>
+        <View style={styles.container}>
+
+        <View style={styles.feather}>
+            <Text style={styles.textcons}>Reportar Aluno</Text>
+
+        </View>
+            <View style={styles.user}>
+                <FontAwesome5 name="user-tag" size={80} color="#0400CD" />
+            </View>
+
+            <FormReportStudent></FormReportStudent>
+
+            <View style={styles.bottons}>
+                <BottonForm nome="Enviar"></BottonForm>
+                <BottonForm nome="Cancelar"></BottonForm>
+
+            </View>
+    
         </View>
     )
 }
+
+const styles = StyleSheet.create ({
+    container:{
+        flex: 1,
+        backgroundColor: '#fff',
+ 
+    },
+    user:{
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginTop: 20
+    },
+    bottons:{
+        flexDirection: 'row',
+        justifyContent: "space-around",
+    },
+    feather:{
+        height: 50,
+        flexDirection: "row",
+        justifyContent:'space-between',
+        paddingVertical: 10,
+        paddingHorizontal: 6,
+        backgroundColor: '#267DFF',
+    },
+    textcons:{
+        fontSize: 25,
+        fontWeight: '500',
+        color: '#fff',
+    },
+})
