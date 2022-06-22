60be340326fdb341a8d3bb3f9bcd5df83883f421
Evely-27 <evelysantos27@gmail.com>
2022-06-22 16:56:06 -0300

    classe para reportar estudantes

---
+++ app_mais_escola/src/class/ClassReportStudents.ts
@@ -0,0 +1,16 @@
+export default class ClassReportStudents {
+    constructor( 
+    
+    public name_student:String ="",
+    public cpf_student:String ="",
+    public endereco_student:String ="",
+    public cep_student:String ="",
+    public num_student:String ="",
+    public tel_student:String ="",
+    public moti_student:String="",
+    public med_student:String=""
+    
+    )
+    
+    {}
+}
